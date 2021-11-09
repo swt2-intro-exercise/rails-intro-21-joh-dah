d82b1a284947d27d4bcb840bf3c8ffb604db6873
joh-dah <johanna@dahlkemper.de>
2021-11-09 10:31:06 +0100

    test

--- spec/factories/authors.rb
+++ spec/factories/authors.rb
@@ -1,7 +1,6 @@
 FactoryBot.define do
   factory :author do
-    # first_name { "Alan" }
-    # last_name { "Turing" }
-    # homepage { "http://wikipedia.de/Alan_Turing" }
+    title { "MyString" }
+    text { "MyText" }
   end
 end
