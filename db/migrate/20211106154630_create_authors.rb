b0fe10f79f11f1a2582310730629ee6be3cae491
joh-dah <johanna@dahlkemper.de>
2021-11-09 11:40:12 +0100

    dsjfk

--- db/migrate/20211106154630_create_authors.rb
+++ db/migrate/20211106154630_create_authors.rb
@@ -1,10 +1,10 @@
-# class CreateAuthors < ActiveRecord::Migration[6.0]
-#   def change
-#     create_table :authors do |t|
-#       t.string :title
-#       t.text :text
+class CreateAuthors < ActiveRecord::Migration[6.0]
+  def change
+    create_table :authors do |t|
+      t.string :title
+      t.text :text
 
-#       t.timestamps
-#     end
-#   end
-# end
+      t.timestamps
+    end
+  end
+end
