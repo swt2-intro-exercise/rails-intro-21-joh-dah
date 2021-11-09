c3f76b83522788b94d1b7b645a1b9196438d5f52
joh-dah <johanna@dahlkemper.de>
2021-11-09 11:28:31 +0100

    second task!!

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,20 +1,4 @@
 class AuthorsController < ApplicationController
-    # def show
-    #     @author = Author.find(params[:id])
-    # end
-
-    # def new
-    # end
-
-    # def create
-    #     @author = Author.new(params.require(:author).permit(:title, :text))
- 
-    #     @author.save
-    #     redirect_to @author
-    # end
-
-    # private
-    # def article_params
-    #     params.require(:article).permit(:title, :text)
-    # end
+    def new
+    end
 end
--- app/helpers/authors_helper.rb
+++ app/helpers/authors_helper.rb
@@ -1,2 +1,7 @@
 module AuthorsHelper
+    
+    def concat_strings(first, second)
+        first + ' ' + second
+    end
+      
 end
