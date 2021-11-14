d179bbbd7ad955f42c2cec49bd85f30544aac42b
joh-dah <johanna@dahlkemper.de>
2021-11-14 14:08:05 +0100

    validation

--- app/models/paper.rb
+++ app/models/paper.rb
@@ -1,4 +1,6 @@
 class Paper < ApplicationRecord
+  validates :title, presence: true
   validates :venue, presence: true
+  validates :year, presence: true, numericality: { only_integer: true }
 
 end
